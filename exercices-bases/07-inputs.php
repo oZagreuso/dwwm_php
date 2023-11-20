ae29d0e61a430c93c2af340bcf4b4277755a4274
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-11-20 16:50:41 +0100

    201123PM1651

--- exercices-bases/07-inputs.php
+++ exercices-bases/07-inputs.php
@@ -13,11 +13,25 @@ function stringLength(string $string)
     }
 }
 
+function passwordCheck(string $string)
+{
+ if(stringLength($string) == false){
+    return false;
+ }
+ if (!preg_match('/[0-9]/', $string)) {
+    return false;
+  }
+  if (!preg_match('/[A-Z]/', $string) || !preg_match('/[a-z]/', $string)) {
+    return false;
+  }
+  if (!preg_match('/[^a-zA-Z0-9]/', $string)) {
+    return false;
+  }
+  return true;
+}
+
 var_dump(stringLength("MotDePasseLong"));
 echo "<br>";
 var_dump(stringLength("azer"));
-
-function passwordCheck(string $string)
-{
-
-}
+echo "<br>";
+var_dump(passwordCheck("MotDePasse1Long$"));
