8d5b0a7e15c3fdcad9ef5287de6e37cdb83ebaf0
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-11-22 10:11:34 +0100

    221123AM1012

--- exercices-bases/08-html.php
+++ exercices-bases/08-html.php
@@ -2,7 +2,6 @@
 
 $names =  ['Joe', 'Jack', 'Léa', 'Zoé', 'Néo' ];
 
-
 function htmlList(string $listName, array $listItems)
 { 
         
@@ -10,25 +9,21 @@ function htmlList(string $listName, array $listItems)
           return "Aucun résultat";
         }      
       
-        sort($listItems);
-
-     
+        sort($listItems);     
 
         $list = "<ul>";
         
         foreach ($listItems as $item) {
          
-            $list .= "<li>{$item}</li>";           
+            $list .= "<li>{$item}</li>";        
             
         }      
 
-         $list .= "</ul>";
+        $list .= "</ul>";
         $title = "<h3>{$listName}</h3>";
 
-        return $title . $list;
-      
+        return $title . $list;      
 }
-
 
 ?>
 <!DOCTYPE html>
@@ -38,12 +33,9 @@ function htmlList(string $listName, array $listItems)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
+<body>    
         
-<?= htmlList("Liste des personnes", $names) ?>
-
- 
+<?= htmlList("Liste des personnes", $names) ?> 
     
 </body>
 </html>
