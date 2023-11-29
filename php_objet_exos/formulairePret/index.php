78fae328e1da63e43f6a2cf70b1edbdd3e9b442e
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-11-29 16:02:37 +0100

    231123PM1603

--- php_objet_exos/formulairePret/index.php
+++ php_objet_exos/formulairePret/index.php
@@ -19,6 +19,7 @@ else {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../formulairePret/style.css">
     <title>Calcul de la mensualité de votre prêt</title>
 </head>
 
@@ -26,26 +27,30 @@ else {
     <main>
     <h1>Formulaire de calcul de la mensualité de votre prêt</h1>
         <form action="<?php  echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            <div>
+        <div class="form">
+            <div class="capital">
                 <label for="capital">Capital emprunté</label>
                 <input type="text" id="capital" name="capital">
             </div>
-            <div>
+            <div class="taux">
                 <label for="taux">Taux d'intérêt annuel</label>
                 <input type="text" id="taux" name="taux">
             </div>
-            <div>
+            <div class="duree">
                 <label for="duree">Durée de remboursement en années</label>
                 <input type="number" id="duree" name="duree">
-            </div>
-            <div>
+            </div>   
+  
+            <div class="submit">
                 <button type="submit">Valider</button>
             </div>
-            <div>
+        </div>
+        <section>
+            <div class="mensualite">
                 <label for="mensualite">Mensualité : </label>
                 <input type="text" readonly id="mensualite" name="mensualite" value="<?php echo $result ; ?>"> 
             </div>
-            
+        </section>
         </form> 
 
 
