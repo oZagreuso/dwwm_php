f38d68ae687b73d078eeb6635a6815280d9d689d
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-12-05 15:48:56 +0100

    051223PM1549

--- php_objet_exos/correctionPret/index.php
+++ php_objet_exos/correctionPret/index.php
@@ -11,7 +11,13 @@
     <?php
     include "./models/Pret.php";
     $monPret = new Pret(10000, 4, 4);
-    echo "mensualité constante :" .$monPret->calculMens()."£";
+
+    //echo "mensualité constante :" .$monPret->calculMens()."€";   
+
+    echo "<caption>Tableau d'amortissement prêt</caption>" . $monPret->tableauAmortissement();
+    $mesdata=$monPret->getTableauAmortissement();
+    $json=json_encode($mesdata,JSON_PRETTY_PRINT);
+    file_put_contents("tableau_pret.json",$json);   
     ?>
 </body>
 </html>
--- php_objet_exos/correctionPret/models/Pret.php
+++ php_objet_exos/correctionPret/models/Pret.php
@@ -32,4 +32,55 @@ class Pret
         $mensualite = ($this->capital * $this->txMens) / $quotient;
         return round($mensualite, 2);
     }
+
+    public function tableauAmortissement() : string
+    {
+        $chaine = "<table><thead><tr><th>nombre de mois</th><th>part intérêt</th><th>part amortissement</th><th>capital restant dû</th><th>mensualité</th></tr></thead><tbody>";
+        $numMois = 1;
+        $capitalRestant = 0;
+        $partInteret = 0;
+        $partAmortissment = 0;
+        $mensualite =  $this->calculMens();
+        do {
+            if ($numMois == 1)
+            {
+                $capitalRestant = $this->capital;
+            }
+            else 
+            {
+                $capitalRestant -= $partAmortissment;
+            }
+            $partInteret = $capitalRestant * $this->txMens;
+            $partAmortissment = $mensualite - $partInteret;
+            $chaine .= "<tr><td>" . $numMois . "</td><td>" . round($partInteret, 2) . "€ </td><td>" .  round($partAmortissment, 2) . "</td><td>" . round($capitalRestant, 2) . "€ </td><td>" . round($mensualite, 2) . "</td></tr>";
+            $numMois ++;
+        } while ($numMois <= $this->nbMois); 
+            $chaine .= "</tbody></table>";
+            return $chaine;
+    }
+    
+            public function getTableauAmortissement():array
+            {
+                $data=array();
+                $partInteret = 0;
+                $partAmortissement = 0;
+                $mensualite =  $this->calculMens();
+                $capitalRestant = $this->capital;
+            for ($i=0; $i < $this->nbMois ; $i++) 
+            { 
+
+            if($i>0)
+            {   
+                $capitalRestant-=$partAmortissement;
+            }
+            $partInteret = $capitalRestant * $this->txMens;
+            $partAmortissement = $mensualite - $partInteret;
+            array_push($data, ["num_mois"=>$i+1,"partInteret"=>round($partInteret,2),"partAmortissement"=>round($partAmortissement),"capital_restant"=>round($capitalRestant,2),"mensualite"=>round($mensualite) ]);
+            }
+
+
+        return $data;
+
+    }
+
 }
