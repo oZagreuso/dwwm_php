db532d3064674ebaf5fdc3fc756bc1fb36d1024c
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-12-06 11:17:25 +0100

    061223AM1118

--- pdo/guideRestaurants/index.php
+++ pdo/guideRestaurants/index.php
@@ -3,23 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Le Guide des meilleurs Restaurants</title>
 </head>
 
 <body>
     <?php
     include "models/Connexion.php";
-    $maConnection = Connexion::getInstance();
-    $request = "SELECT * FROM restaurants";
-    $state = $maConnection->prepare($request);
-    $state->execute();
-        //on affiche le résultat de la requête, ici sous forme de tableau
-        $tableau = [];
-        while ($ligne = $state->fetch())
-            {
-                var_dump($ligne);
-            }
+    include "models/MyTable.php";
     
+        //on affiche le résultat de la requête, ici sous forme de tableau
+     
+            $table  =  new MyTable('restaurants');
+            $table->searchInBdd();
+            var_dump($table->rendreHTML($table));
+
+           
+
+
+    
+         
     ?>
 </body>
 </html>
---
+++ pdo/guideRestaurants/models/MyTable.php
@@ -0,0 +1,64 @@
+<?php
+
+//include "models/Connexion.php";
+
+class MyTable
+{
+    private string $base;
+
+
+    public function __construct(string $base)
+    {
+        $this->base = $base;
+    }
+
+    public function searchInBdd()
+    {
+        $bdd = [];
+        try {
+            $maConnection = Connexion::getInstance();
+            $request = "SELECT * FROM " . $this->base;
+            $state = $maConnection->prepare($request);
+            $state->execute();
+            $bdd = $state->fetchAll();
+        } catch (PDOException $e) {
+            die("!! data not found !!" . $e ->getMessage());
+        }   
+
+        return $bdd;
+
+    }
+
+    public function rendreHTML() 
+    {        
+        $resultats = $this->searchInBdd();
+        echo '<table border="1">';
+        echo '<tr>';
+        echo '<th>id</th>';
+        echo '<th>nom</th>';
+        echo '<th>adresse</th>';
+        echo '<th>prix</th>';
+        echo '<th>commentaire</th>';
+        echo '<th>note</th>';
+        echo '<th>visite</th>';
+        echo '</tr>';
+      $resultats = $this->searchInBdd();
+      foreach($resultats as $row)
+      {
+        echo '<tr>';
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td>' . $row['nom'] . '</td>';
+        echo '<td>' . $row['adresse'] . '</td>';
+        echo '<td>' . $row['prix'] . '</td>';
+        echo '<td>' . $row['commentaire'] . '</td>';
+        echo '<td>' . $row['note'] . '</td>';
+        echo '<td>' . $row['visite'] . '</td>';
+        echo '</tr>';
+       
+      }
+    }
+        
+         
+    
+}
+
