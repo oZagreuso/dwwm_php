db53d42affe39c0227ceec93ca7cc3b458272045
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2024-01-04 08:11:02 +0100

    010124AM0810

--- colBlanc/colblanc.php
+++ colBlanc/colblanc.php
@@ -1,16 +1,20 @@
 <!doctype html>
-<html lang="Fr">
+<html lang="fr-fr">
 <head>
   <meta charset="utf-8">
   <title>Entrainement Centre de Readaptation</title>
-  <link rel="stylesheet" media="screen" href="css/style.css">
+  <link rel="stylesheet" media="screen" href="../css/style.css">
 </head>	
+
 <body>
 
         <div id="page">
-         <div id="header">
-          <img src="contenu/header.jpg" width="980" height="176" alt="colblanc entete"> 
-        </div>
+            <header>
+              <div id="header">
+                <img src="../contenu/header.jpg" width="980" height="176" alt="colblanc entete"> 
+                </div>
+            </header>
+         
 
         <div id="menu">
           <ul>
@@ -36,31 +40,47 @@
            </li>
          </ul></li>
          <li><a href="#">Projets</a>
-
          </li>
          <li><a href="#">offres</a>
           <ul>
-
             <li><a href="#">Par secteur</a>
-
             </li>
-
             <li><a href="#">Par entreprises</a>
-
             </li>
           </ul>
         </li>
       </ul>
     </div>
-
-
-
-
+    
     <main>
-      <section> 
-
-     
-     
+      <section class="form"> 
+        <form action ="<?php  echo $_SERVER['PHP_SELF']; ?>" method="POST">
+          <div class="departement">
+            <label for="departement">Choisissez votre département</label>
+            <select id="departement" name="departement">
+          </div>
+          <div class="etablissement">
+            <label for="etablissement">Sélectionnez votre type d'établissement</label>            
+            <input type="checkbox" id="TPE" name="TPE">
+            <label for="TPE">TPE</label>
+            <input type="checkbox" id="PME" name="PME">
+            <label for="PME">PME</label>
+            <input type="checkbox" id="grdeEntr" name="grdeEntr">
+            <label for="grdeEntr">GRANDE ENTREPRISE</label>
+            <input type="checkbox" id="colTer" name="colTer">
+            <label for="colTer">COLLECTIVITES TER</label>
+            <input type="checkbox" id="assoc" name="assoc">
+            <label for="assoc">ASSOCIATION</label>
+            <input type="checkbox" id="autres" name="autres">
+            <label for="autres">AUTRES </label>
+          </div>
+          <div class="submit">
+              <button type="submit">Valider</button>
+          </div>
+        </form>
+          <div class="print">
+            <button type="submit" onclick="window.print()">Imprimer</button>
+          </div>
       <h1 style=" text-align:center">Votre travail ici</h1>
   <?php
 
@@ -68,8 +88,8 @@
   ?>
    <aside>
 
-</aside>
- </section>
+  </aside>
+      </section>
     </main>
 
 
